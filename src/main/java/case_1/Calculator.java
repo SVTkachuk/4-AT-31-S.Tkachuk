b6a210ccb1d8dd4e2d5e8df7911a1ab6caaf758c
Sv <sviatik.tkachuk@gmail.com>
2022-02-11 21:29:21 +0200

    test
    vf
    
    ':

--- src/main/java/case_1/Calculator.java
+++ src/main/java/case_1/Calculator.java
@@ -1,34 +1,40 @@
 package case_1;
 
-
+import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         //#Primitives
         //Task 1. Implement a simple calculator with 4 operations. Each operation - different method. Use it in the main method.
+        System.out.println("For float value use ',' example: 4,4");
+        Scanner obj = new Scanner(System.in);
+        System.out.println("first number:");
+        double a= obj.nextDouble();
+        System.out.println("second number:");
+        double b= obj.nextDouble();
 
-
-        int a=1;
-        int b=2;
-
+        System.out.printf(a+" + "+b+" = ");
         System.out.println(plus(a,b));
+        System.out.printf(a+" - "+b+" = ");
         System.out.println(minus(a,b));
+        System.out.printf(a+" * "+b+" = ");
         System.out.println(times(a,b));
+        System.out.printf(a+" / "+b+" = ");
         System.out.println(dividedBy(a,b));
     }
 
     private static double dividedBy(double a, double b) {
         return a/b;
-    };
+    }
 
     private static double times(double a, double b) {
         return a*b;
-    };
+    }
 
     private static double minus(double a, double b) {
         return a-b;
-    };
+    }
 
     private static double plus(double a, double b) {
         return a+b;
-    };
+    }
 }
