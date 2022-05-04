f3ea17975e5255d23b22344010570d4776d10bd0
Sv <sviatik.tkachuk@gmail.com>
2022-05-04 18:13:42 +0300

    some functions with web elements

--- src/main/java/case_10/ChromeMain.java
+++ src/main/java/case_10/ChromeMain.java
@@ -2,12 +2,17 @@ package case_10;
 
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.time.Duration;
+import java.util.List;
 
 
 public class ChromeMain {
@@ -25,20 +30,18 @@ public class ChromeMain {
         tags.click();
         //filter tags
        WebElement tagFilter = driver.findElement(By.xpath("//*[@id=\"tagfilter\"]"));
-       tagFilter.sendKeys("java");
-       //chose the first related tag
-        Thread.sleep(2000);
-        WebElement firstTag = driver.findElement(By.xpath("//*[@id=\"tags-browser\"]/div[1]/div[1]/div/a"));
-        firstTag.click();
-        //filter questions
-        WebElement questionFilter = driver.findElement(By.xpath("//*[@id=\"mainbar\"]/div[4]/div/div[2]/div/div[3]/button"));
-        questionFilter.click();
-        //reset tags
-        WebElement tagEditor = driver.findElement(By.xpath("//*[@id=\"uql-form\"]/div/div/div[1]/div/div[3]/div/div/span[1]/span/a"));
-        tagEditor.click();
-        //apply filter
-        WebElement filterApplyButton = driver.findElement(By.xpath("//*[@id=\"uql-form\"]/div/div/div[2]/div/div[1]/button"));
-        filterApplyButton.click();
+       String key = "java-threadssadasd";
+       tagFilter.sendKeys(key);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        WebElement tag = null;
+        try{
+         tag = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(String.format("//a[text()='%s']",key ))));
+        }
+        catch (TimeoutException e){
+            System.out.println("TimeOut");
+        }
+        tag.click();
+
 
     }
 }
