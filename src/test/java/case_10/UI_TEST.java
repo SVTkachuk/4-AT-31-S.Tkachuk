5c701fae79c6daea2f89e8b1eeb897569eee256d
Sv <sviatik.tkachuk@gmail.com>
2022-05-09 12:53:11 +0300

    UI-Test

--- src/test/java/case_10/UI_TEST.java
+++ src/test/java/case_10/UI_TEST.java
@@ -1,5 +1,6 @@
 package case_10;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
@@ -9,40 +10,44 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class UI_TEST{
+
 @Test
-   void StackOverFlow(){
-    String key = "java";
-    ChromeDriverManager.getInstance().setup();
-    WebDriver driver = new ChromeDriver();
-    //go to the site
-    driver.get("https://stackoverflow.com/");
-    //go to tags page
-    WebElement burgerMenu =  driver.findElement(By.xpath("/html/body/header/div/a[1]"));
-    burgerMenu.click();
-    WebElement tags = driver.findElement(By.xpath("//*[@id=\"nav-tags\"]"));
-    tags.click();
-    //filter tags
-    WebElement tagFilter = driver.findElement(By.xpath("//*[@id=\"tagfilter\"]"));
-    tagFilter.sendKeys(key);
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-    WebElement tag = null;
-    try{
-        tag = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(String.format("//a[text()='%s']",key ))));
-    }
-    catch (TimeoutException e){
-        System.out.println("TimeOut");
-        driver.quit();
-    }
-    tag.click();
-    //Verify if the tag key is equal to tag title
-    WebElement tagTitle =driver.findElement(By.xpath("//*[@id=\"mainbar\"]/div[1]/div[1]/h1"));
-    Assert.assertEquals(String.format("Questions tagged [%s]",key),tagTitle.getText(),"wrong tag");
-    driver.quit();
+   void TagKeys() {
+ ChromeDriverManager.getInstance().setup();
+ WebDriver driver = new ChromeDriver();
+ StackOverFlowTags("java",driver);
+ StackOverFlowTags("javascript",driver);
+ StackOverFlowTags("python",driver);
+ driver.quit();
+}
+    void StackOverFlowTags(String key, WebDriver driver){
+     //go to the site
+     driver.get("https://stackoverflow.com/");
+     //go to tags page
+     WebElement burgerMenu =  driver.findElement(By.xpath("/html/body/header/div/a[1]"));
+     burgerMenu.click();
+     WebElement tags = driver.findElement(By.xpath("//*[@id=\"nav-tags\"]"));
+     tags.click();
+     //filter tags
+     WebElement tagFilter = driver.findElement(By.xpath("//*[@id=\"tagfilter\"]"));
+     tagFilter.sendKeys(key);
+     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+     WebElement tag = null;
+     try{
+      tag = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(String.format("//a[text()='%s']",key ))));
+     }
+     catch (TimeoutException e){
+      System.out.println("TimeOut");
+      driver.quit();
+     }
+     tag.click();
+     //Verify if the tag key is equal to tag title
+     WebElement tagTitle =driver.findElement(By.xpath("//*[@id=\"mainbar\"]/div[1]/div[1]/h1"));
+     Assert.assertEquals(String.format("Questions tagged [%s]",key),tagTitle.getText(),"wrong tag");
     }
 }
